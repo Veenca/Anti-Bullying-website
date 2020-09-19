dea9108eeb316647186edc77831b152f0b856da9
Veenca <63163300+Veenca@users.noreply.github.com>
2020-09-19 16:05:28 +0200

    when click a disease the div tag expands

--- scripts/file.js
+++ scripts/file.js
@@ -1,8 +1,35 @@
+
+
 $(document).ready(function () {
-   let info = $('.dont-show');
-   let infoSum =$('.toggle-element');
-   infoSum.hover(function(){
-        info.removeClass('dont-show');
-   });
+
+   let infoSum =$('.main-cicatrici');
+   infoSum.siblings('p').hide();
+   let bg=$('li.toggle-element');
+   let hstart=bg.css("height");
+ 
+
+   infoSum.click(function(){
+      if(!$(this).siblings('p').hasClass('xx')){
+        $(this).siblings('p').fadeIn(500);
+      bg.css("height","fit-content");
+      
+       $(this).css("text-decoration","none");
+       $(this).siblings('p').addClass('xx');
+      }else{ 
+       
+       $(this).css("text-decoration","underline");
+       $(this).siblings('p').removeClass('xx');
+       $(this).siblings('p').fadeOut(200);
+        bg.animate({
+           "heigth":hstart
+        },{
+           duration: 250
+        });
+        
+      }
    
+     });
+ 
 });
+
+ 
